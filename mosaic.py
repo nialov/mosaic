0bcac3a391c2676b8332ed432c754d61ddd4adc3
nialov <nikolasovaskainen@gmail.com>
2023-07-14 15:16:19 +0300

    feat(mosaic): improve cli reporting

--- mosaic.py
+++ mosaic.py
@@ -1,31 +1,42 @@
 import logging
-import os
-import os.path
 import sys
 from multiprocessing import Process, Queue, cpu_count
 from pathlib import Path
 
 import typer
 from PIL import Image, ImageOps
+from rich import print
+from rich.progress import Progress, track
 
 # Change these 3 config parameters to suit your needs...
-TILE_SIZE = 50  # height/width of mosaic tiles in pixels
-TILE_MATCH_RES = 5  # tile matching resolution (higher values give better fit but require more processing)
-ENLARGEMENT = (
+DEFAULT_TILE_SIZE = 50  # height/width of mosaic tiles in pixels
+DEFAULT_TILE_MATCH_RES = 5  # tile matching resolution (higher values give better fit but require more processing)
+DEFAULT_ENLARGEMENT = (
     2  # the mosaic image will be this many times wider and taller than the original
 )
 
-TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
+# TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
 WORKER_COUNT = max(cpu_count() - 1, 1)
-OUT_FILE = "mosaic.jpeg"
 EOQ_VALUE = None
 
 APP = typer.Typer()
 
 
+def _tile_block_size(tile_size, tile_match_res):
+    return tile_size / max(min(tile_match_res, tile_size), 1)
+
+
 class TileProcessor:
-    def __init__(self, tiles_directory):
+    def __init__(self, tiles_directory, tile_size, tile_match_res):
         self.tiles_directory = str(Path(tiles_directory).resolve())
+        self.tile_size = tile_size
+        self.tile_match_res = tile_match_res
+
+    @property
+    def tile_block_size(self):
+        return _tile_block_size(
+            tile_size=self.tile_size, tile_match_res=self.tile_match_res
+        )
 
     def __process_tile(self, tile_path):
         try:
@@ -40,9 +51,12 @@ class TileProcessor:
             h_crop = (h - min_dimension) / 2
             img = img.crop((w_crop, h_crop, w - w_crop, h - h_crop))
 
-            large_tile_img = img.resize((TILE_SIZE, TILE_SIZE), Image.LANCZOS)
+            large_tile_img = img.resize((self.tile_size, self.tile_size), Image.LANCZOS)
             small_tile_img = img.resize(
-                (int(TILE_SIZE / TILE_BLOCK_SIZE), int(TILE_SIZE / TILE_BLOCK_SIZE)),
+                (
+                    int(self.tile_size / self.tile_block_size),
+                    int(self.tile_size / self.tile_block_size),
+                ),
                 Image.LANCZOS,
             )
 
@@ -63,14 +77,16 @@ class TileProcessor:
             large_tile, small_tile = self.__process_tile(image_path)
             return large_tile, small_tile
 
-        for image_path_unresolved in Path(self.tiles_directory).iterdir():
-            image_path = image_path_unresolved.resolve()
-            assert image_path.exists(), image_path
-            print("Reading {:40.40}".format(str(image_path)), flush=True, end="\r")
-            large_tile, small_tile = _process_image(image_path=image_path)
-            if all(tile is not None for tile in (large_tile, small_tile)):
-                large_tiles.append(large_tile)
-                small_tiles.append(small_tile)
+        with Progress() as progress:
+            task = progress.add_task("Reading tiles", total=None)
+            for image_path_unresolved in Path(self.tiles_directory).iterdir():
+                image_path = image_path_unresolved.resolve()
+                assert image_path.exists(), image_path
+                large_tile, small_tile = _process_image(image_path=image_path)
+                if all(tile is not None for tile in (large_tile, small_tile)):
+                    large_tiles.append(large_tile)
+                    small_tiles.append(small_tile)
+                progress.update(task, description=image_path.name)
 
         print("Processed {} tiles.".format(len(large_tiles)))
 
@@ -78,24 +94,34 @@ class TileProcessor:
 
 
 class TargetImage:
-    def __init__(self, image_path):
+    def __init__(self, image_path, enlargement, tile_size, tile_match_res):
         self.image_path = image_path
+        self.enlargement = enlargement
+        self.tile_size = tile_size
+        self.tile_match_res = tile_match_res
+
+    @property
+    def tile_block_size(self):
+        return _tile_block_size(
+            tile_size=self.tile_size, tile_match_res=self.tile_match_res
+        )
 
     def get_data(self):
         print("Processing main image...")
         img = Image.open(self.image_path)
-        w = img.size[0] * ENLARGEMENT
-        h = img.size[1] * ENLARGEMENT
+        w = img.size[0] * self.enlargement
+        h = img.size[1] * self.enlargement
         large_img = img.resize((w, h), Image.LANCZOS)
-        w_diff = (w % TILE_SIZE) / 2
-        h_diff = (h % TILE_SIZE) / 2
+        w_diff = (w % self.tile_size) / 2
+        h_diff = (h % self.tile_size) / 2
 
         # if necessary, crop the image slightly so we use a whole number of tiles horizontally and vertically
         if w_diff or h_diff:
             large_img = large_img.crop((w_diff, h_diff, w - w_diff, h - h_diff))
 
         small_img = large_img.resize(
-            (int(w / TILE_BLOCK_SIZE), int(h / TILE_BLOCK_SIZE)), Image.LANCZOS
+            (int(w / self.tile_block_size), int(h / self.tile_block_size)),
+            Image.LANCZOS,
         )
 
         image_data = (large_img.convert("RGB"), small_img.convert("RGB"))
@@ -157,29 +183,16 @@ def fit_tiles(work_queue, result_queue, tiles_data):
     result_queue.put((EOQ_VALUE, EOQ_VALUE))
 
 
-class ProgressCounter:
-    def __init__(self, total):
-        self.total = total
-        self.counter = 0
-
-    def update(self):
-        self.counter += 1
-        print(
-            "Progress: {:04.1f}%".format(100 * self.counter / self.total),
-            flush=True,
-            end="\r",
-        )
-
-
 class MosaicImage:
-    def __init__(self, original_img):
+    def __init__(self, original_img, tile_size):
         self.image = Image.new(original_img.mode, original_img.size)
-        self.x_tile_count = int(original_img.size[0] / TILE_SIZE)
-        self.y_tile_count = int(original_img.size[1] / TILE_SIZE)
+        self.x_tile_count = int(original_img.size[0] / tile_size)
+        self.y_tile_count = int(original_img.size[1] / tile_size)
         self.total_tiles = self.x_tile_count * self.y_tile_count
+        self.tile_size = tile_size
 
     def add_tile(self, tile_data, coords):
-        img = Image.new("RGB", (TILE_SIZE, TILE_SIZE))
+        img = Image.new("RGB", (self.tile_size, self.tile_size))
         img.putdata(tile_data)
         self.image.paste(img, coords)
 
@@ -188,9 +201,9 @@ class MosaicImage:
 
 
 def build_mosaic(
-    result_queue, all_tile_data_large, original_img_large, output_image: Path
+    result_queue, all_tile_data_large, original_img_large, output_image: Path, tile_size
 ):
-    mosaic = MosaicImage(original_img_large)
+    mosaic = MosaicImage(original_img_large, tile_size=tile_size)
 
     active_workers = WORKER_COUNT
     while True:
@@ -209,55 +222,63 @@ def build_mosaic(
             pass
 
     mosaic.save(output_image)
-    print("\nFinished, output is in", output_image)
 
 
-def compose(original_img, tiles, output_image: Path):
+def compose(original_img, tiles, output_image: Path, tile_size, tile_block_size):
     print("Building mosaic, press Ctrl-C to abort...")
     original_img_large, original_img_small = original_img
     tiles_large, tiles_small = tiles
 
-    mosaic = MosaicImage(original_img_large)
+    mosaic = MosaicImage(original_img_large, tile_size=tile_size)
 
     all_tile_data_large = [list(tile.getdata()) for tile in tiles_large]
     all_tile_data_small = [list(tile.getdata()) for tile in tiles_small]
 
     work_queue = Queue(WORKER_COUNT)
     result_queue = Queue()
+    build_mosaic_process = Process(
+        target=build_mosaic,
+        args=(
+            result_queue,
+            all_tile_data_large,
+            original_img_large,
+            output_image,
+            tile_size,
+        ),
+    )
 
     try:
         # start the worker processes that will build the mosaic image
-        Process(
-            target=build_mosaic,
-            args=(result_queue, all_tile_data_large, original_img_large, output_image),
-        ).start()
+        build_mosaic_process.start()
 
         # start the worker processes that will perform the tile fitting
+        worker_processes = []
         for n in range(WORKER_COUNT):
-            Process(
+            worker_process = Process(
                 target=fit_tiles, args=(work_queue, result_queue, all_tile_data_small)
-            ).start()
+            )
+            worker_process.start()
+            worker_processes.append(worker_process)
 
-        progress = ProgressCounter(mosaic.x_tile_count * mosaic.y_tile_count)
-        for x in range(mosaic.x_tile_count):
+        for x in track(range(mosaic.x_tile_count), "Processing..."):
             for y in range(mosaic.y_tile_count):
                 large_box = (
-                    x * TILE_SIZE,
-                    y * TILE_SIZE,
-                    (x + 1) * TILE_SIZE,
-                    (y + 1) * TILE_SIZE,
+                    x * tile_size,
+                    y * tile_size,
+                    (x + 1) * tile_size,
+                    (y + 1) * tile_size,
                 )
                 small_box = (
-                    x * TILE_SIZE / TILE_BLOCK_SIZE,
-                    y * TILE_SIZE / TILE_BLOCK_SIZE,
-                    (x + 1) * TILE_SIZE / TILE_BLOCK_SIZE,
-                    (y + 1) * TILE_SIZE / TILE_BLOCK_SIZE,
+                    x * tile_size / tile_block_size,
+                    y * tile_size / tile_block_size,
+                    (x + 1) * tile_size / tile_block_size,
+                    (y + 1) * tile_size / tile_block_size,
                 )
                 work_queue.put(
                     (list(original_img_small.crop(small_box).getdata()), large_box)
                 )
-                progress.update()
-        return result_queue
+
+        return build_mosaic_process
 
     except KeyboardInterrupt:
         print("\nHalting, saving partial image please wait...")
@@ -268,18 +289,32 @@ def compose(original_img, tiles, output_image: Path):
             work_queue.put((EOQ_VALUE, EOQ_VALUE))
 
 
-def show_error(msg):
-    print("ERROR: {}".format(msg))
-
-
-def mosaic(img_path, tiles_path, output_image: Path):
-    image_data = TargetImage(img_path).get_data()
-    tiles_data = TileProcessor(tiles_path).get_tiles()
+def mosaic(
+    img_path, tiles_path, output_image: Path, tile_size, tile_match_res, enlargement
+):
+    target_image = TargetImage(
+        img_path,
+        enlargement=enlargement,
+        tile_size=tile_size,
+        tile_match_res=tile_match_res,
+    )
+    image_data = target_image.get_data()
+    tiles_data = TileProcessor(
+        tiles_path, tile_size=tile_size, tile_match_res=tile_match_res
+    ).get_tiles()
     if tiles_data[0]:
-        result_queue = compose(image_data, tiles_data, output_image=output_image)
-        return result_queue
+        build_mosaic_process = compose(
+            image_data,
+            tiles_data,
+            output_image=output_image,
+            tile_size=tile_size,
+            tile_block_size=target_image.tile_block_size,
+        )
+        return build_mosaic_process
     else:
-        show_error("No images found in tiles directory '{}'".format(tiles_path))
+        raise FileNotFoundError(
+            "Expected images in tiles directory {}".format(tiles_path)
+        )
 
 
 @APP.command()
@@ -301,11 +336,34 @@ def main(
         help="The directory with image tiles.",
     ),
     output_image: Path = typer.Option(
-        Path(OUT_FILE),
+        Path("mosaic.jpeg"),
         help="Output image path.",
     ),
+    tile_size: int = typer.Option(
+        DEFAULT_TILE_SIZE,
+        help="Height/width of mosaic tiles in pixels.",
+    ),
+    tile_match_res: int = typer.Option(
+        DEFAULT_TILE_MATCH_RES,
+        help="Tile matching resolution (higher values give better fit but require more processing).",
+    ),
+    enlargement: int = typer.Option(
+        DEFAULT_ENLARGEMENT,
+        help="The mosaic image will be this many times wider and taller than the original.",
+    ),
 ):
-    mosaic(img_path=target_image, tiles_path=tile_dir, output_image=output_image)
+    build_mosaic_process = mosaic(
+        img_path=target_image,
+        tiles_path=tile_dir,
+        output_image=output_image,
+        tile_size=tile_size,
+        tile_match_res=tile_match_res,
+        enlargement=enlargement,
+    )
+    assert build_mosaic_process is not None
+    # Join the mosaic building process
+    build_mosaic_process.join()
+    print("\nFinished, output is in {}".format(output_image))
 
 
 if __name__ == "__main__":
--- tests/test_mosaic.py
+++ tests/test_mosaic.py
@@ -14,20 +14,20 @@ NIXOS_IMAGE_PATH = LIBRARY_1_PATH / "nixos_logo.png"
 @pytest.mark.parametrize("target_image, tile_dir", [(NIXOS_IMAGE_PATH, LIBRARY_1_PATH)])
 def test_main(target_image, tile_dir, tmp_path: Path, data_regression):
     output_image = tmp_path / "output.png"
-    result_queue = mosaic.mosaic(
-        img_path=target_image, tiles_path=tile_dir, output_image=output_image
+    build_mosaic_process = mosaic.mosaic(
+        img_path=target_image,
+        tiles_path=tile_dir,
+        output_image=output_image,
+        tile_size=mosaic.DEFAULT_TILE_SIZE,
+        tile_match_res=mosaic.DEFAULT_TILE_MATCH_RES,
+        enlargement=mosaic.DEFAULT_ENLARGEMENT,
     )
-    assert result_queue is not None
-    counter = count()
-    while not output_image.exists():
-        current_count = next(counter)
-        print(current_count)
-        time.sleep(0.01)
-        if current_count > 100:
-            raise TimeoutError("Expected image to be compiled in 1 second.")
-
-    assert result_queue.empty(), result_queue
+    assert build_mosaic_process is not None
+    # Join the mosaic building process
+    build_mosaic_process.join()
+    # assert result_queue.empty(), result_queue
     assert output_image.exists(), output_image
+
     image_bytes = output_image.read_bytes()
     sha256_hash = sha256()
     sha256_hash.update(image_bytes)
