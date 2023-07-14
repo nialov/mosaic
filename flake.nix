07d717084db20e73dafc64eafbd849ca6864dfd6
nialov <nikolasovaskainen@gmail.com>
2023-07-14 13:25:47 +0300

    ci: add ci

--- flake.nix
+++ flake.nix
@@ -29,22 +29,10 @@
             inputsFrom = [ self.packages."${system}".mosaic-dev ];
           };
         };
+        checks = { inherit (pkgs) mosaic-dev; };
       })) // {
 
         overlays.default = final: prev: {
-          # mosaic-dev = final.mosaic.overrideAttrs (finalAttrs: prevAttrs: {
-          #   pname = "mosaic";
-          #   version = "unstable-2023-07-13";
-          #   format = "pyproject";
-          #   src = prev.poetry2nix.cleanPythonSources { src = ./.; };
-          #   checkInputs = with prev.python3.pkgs; [ pytestCheckHook pytest ];
-
-          #   pythonImportsCheck = [ "mosaic" ];
-          #   propagatedBuildInputs = prevAttrs.propagatedBuildInputs
-          #     ++ [ prev.python3Packages.typer ]
-          #     ++ prev.python3Packages.typer.passthru.optional-dependencies.all;
-          #   nativeBuildInputs = with prev.python3.pkgs; [ setuptools wheel ];
-          # });
           mosaic-dev = prev.callPackage ./. { };
 
         };
